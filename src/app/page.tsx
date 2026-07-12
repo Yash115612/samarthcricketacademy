39c4ba26c1c7555f020d48d7545a1c9623064820
Yash115612 <155476660+Yash115612@users.noreply.github.com>
2026-07-12 20:38:24 +0530

    perf: cache landing page for 1 hour, remove force-dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import { HeroSlider } from "@/components/home/HeroSlider";
 import { AutoScrollGallery } from "@/components/home/AutoScrollGallery";
 import { Base64VideoPlayer } from "@/components/home/Base64VideoPlayer";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 3600;
 
 export default async function Home() {
   await ensureDbSynced();
